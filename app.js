51d9a727755f0ab1b60b6bd05c7ab07999d8ffe2
Nowrose-Irab-Poll <nowroseirabpoll@gmail.com>
2022-02-05 23:08:32 +0600

    enter button works

---
+++ app.js
@@ -0,0 +1,24 @@
+const btnElement = document.querySelector("button");
+const inputElement = document.querySelector("input");
+const list = document.querySelector("ul");
+
+const onAddGoalPressed = () => {
+  const textInput = inputElement.value;
+
+  const listElemnt = document.createElement("li");
+
+  listElemnt.textContent = textInput;
+
+  list.appendChild(listElemnt);
+
+  inputElement.value = "";
+};
+
+btnElement.addEventListener("click", onAddGoalPressed);
+
+inputElement.addEventListener("keyup", () => {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    onAddGoalPressed();
+  }
+});
